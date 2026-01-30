0209388577faec0f093d6b7a7f7a5358320318a0
ben.hib <ben.hibinger@gmail.com>
2026-01-30 20:52:10 +0100

    Lab_II exercise

---
+++ courses/coding-in-C/Lab_2/chessboard.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+char col_lable[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
+
+int main () {
+
+    chessboard_II();
+
+    return 0;
+}
+
+void chessboard_I() {
+
+    for (int col = 8; col > 0; col--) //field starts at 8
+    {
+        for (int row = 1; row < 9; row++)
+        {
+            printf("%c%i ", col_lable[row-1], col);
+        }
+        printf("\n");
+    }
+    
+}
+
+void chessboard_II() {
+    
+    char black[] = "###";
+    char white[] = "   ";
+
+    row_letter();
+    row_border();
+
+    for (int row = 8; row > 0; row--)
+    {
+        printf("%i ", row);
+        if (!(row%2)) {
+                for (int i = 0; i < 4; i++)
+        {
+            printf("|%s|", black);
+            printf("%s", white);
+        }} else {
+            for (int i = 0; i < 4; i++)
+        {
+            printf("|%s|", white);
+            printf("%s", black);
+        }
+        
+    }
+    printf("| %i\n", row);
+    }
+    
+
+    row_border();
+    row_letter();
+
+}
+
+void row_letter() {
+    printf(" ");
+    for (int col = 0; col < 9; col++) //Letters on top
+    {
+            printf("%4c", col_lable[col]);
+    }
+    printf("\n");
+}
+
+void row_border() {
+    char pattern[] = "+---";
+    printf("  ");
+    for (int i = 0; i < 8; i++)
+    {
+        printf("%s", pattern);
+    }
+    printf("+  \n");
+}
