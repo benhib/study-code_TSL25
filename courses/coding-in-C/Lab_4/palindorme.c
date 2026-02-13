1e9355b9b28348eeeb757381e90488e282c77fce
Ben.Hib <ben.hibinger@gmail.com>
2026-02-13 09:20:08 +0100

    small fix

--- courses/coding-in-C/Lab_4/palindorme.c
+++ courses/coding-in-C/Lab_4/palindorme.c
@@ -16,19 +16,7 @@ int main() {
         }
     }
     
-    if(i%2) { // Ist die Länge ungerade?
-        int mid = i - i/2;
-        printf("%i\n", mid);
-        for (int pos = 1; pos <= i/2; pos++) {
-            if (tolower(word[(mid-pos)-1]) == tolower(word[(mid+pos)-1])) { // Prüft auf Gleichheit von der Mitte des Worts aus
-                continue;
-            } 
-            printf("Das Wort %s ist kein Palindrom.\n", word);
-            return 0;
-        }
-        printf("Das Wort %s ist ein Palindrom. \n", word);
-
-    } else {
+    
         for (int pos = 0; pos <= i/2; pos++) { // Prüft auf Gleichheit vom Rand des Worts aus
             if (tolower(word[0+pos]) == tolower(word[i-pos-1])) {
                 continue;
@@ -37,7 +25,7 @@ int main() {
             return 0;
         }
         printf("Das Wort %s ist ein Palindrom. \n", word);
-    }
+    
 
 
 
