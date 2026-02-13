c8c4f3bec6b839858b38b4f125b9a3e1bdae25fd
Ben.Hib <ben.hibinger@gmail.com>
2026-02-13 09:34:48 +0100

    strlen

---
+++ courses/coding-in-C/Lab_4/palindrom.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char word[101];
+
+    printf("Geben Sie ein Wort zum testen ein: \n");
+    scanf("%100s", word);
+/*
+fgets nutzen:
+if(!fgets(word, sizeof(word), stdin)) { ERROR }
+word[strscn(word, "\n") = "\0"];
+
+
+*/
+    
+    int i = 0;
+    for (;i < 101; i++) { // Überprüft Länge des Worts, geht mit strlen(word);
+        if (word[i] == '\0') {
+            i;
+            break;
+        }
+    }
+    
+    
+        for (int pos = 0; pos <= i/2; pos++) { // Prüft auf Gleichheit vom Rand des Worts aus
+            if (tolower(word[0+pos]) == tolower(word[i-pos-1])) {
+                continue;
+            } 
+            printf("Das Wort %s ist kein Palindrom.\n", word);
+            return 0;
+        }
+        printf("Das Wort %s ist ein Palindrom. \n", word);
+    
+
+
+
+    return 0;
+}
